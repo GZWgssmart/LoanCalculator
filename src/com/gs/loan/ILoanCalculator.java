6ddd1b8b8614aebf6504eb6115954a20ffe49530
WangGenshen <WangGenshen@WangGenshens-MacBook-Pro.local>
2016-01-23 10:44:50 +0800

    加入适配器模式，便于扩展

--- src/com/gs/loan/ILoanCalculator.java
+++ src/com/gs/loan/ILoanCalculator.java
@@ -8,22 +8,14 @@ import java.math.BigDecimal;
 public interface ILoanCalculator {
 
     /**
-     * 等额本息还款法
+     * 贷款计算
+     *
      * @param totalLoanMoney 总贷款额
      * @param totalMonth 还款月数
      * @param loanRate 贷款利率
      * @param rateType 可选择年利率或月利率
      * @return
      */
-    public Loan calculateOne(BigDecimal totalLoanMoney, int totalMonth, double loanRate, int rateType);
+    public Loan calLoan(BigDecimal totalLoanMoney, int totalMonth, double loanRate, int rateType);
 
-    /**
-     * 等额本金还款法
-     * @param totalLoanMoney 总贷款额
-     * @param totalMonth 还款月数
-     * @param loanRate 贷款利率
-     * @param rateType 可选择年利率或月利率
-     * @return
-     */
-    public Loan calculateTwo(BigDecimal totalLoanMoney, int totalMonth, double loanRate, int rateType);
 }
--- src/com/gs/loan/LoanCalculatorTest.java
+++ src/com/gs/loan/LoanCalculatorTest.java
@@ -1,29 +1,50 @@
 package com.gs.loan;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 /**
  * Created by WangGenshen on 1/14/16.
  */
-public class LoanCalculatorTest {
+public class LoanCalculatorTest extends TestCase {
+
+    private int totalMonth;
+    private BigDecimal totalMoney;
+    private double percent;
+    private double rate;
+    private double rateDiscount;
+
+    @Override
+    protected void setUp() throws Exception {
+        totalMonth = 36;
+        totalMoney = new BigDecimal(50000);
+        percent = 0;
+        rate = 10.8;
+        rateDiscount = 1;
+    }
 
     @Test
-    public void testCalculate() {
-        int totalMonth = 360;
-        BigDecimal totalMoney = new BigDecimal(510000);
-        double percent = 0.3;
-        double rate = 4.9;
-        double rateDiscount = 0.85;
-
-        LoanCalculator calculator = new LoanCalculator();
-        Loan loan = calculator.calculateOne(
-                LoanCalculator.totalLoanMoney(totalMoney, percent),
+    public void testACPIMCalculate() {
+        ACPIMLoanCalculator calculator = new ACPIMLoanCalculator();
+        Loan loan = calculator.calLoan(
+                LoanUtil.totalLoanMoney(totalMoney, percent),
                 totalMonth,
-                LoanCalculator.rate(rate, rateDiscount),
-                LoanCalculator.RATE_TYPE_YEAR);
+                LoanUtil.rate(rate, rateDiscount),
+                LoanUtil.RATE_TYPE_YEAR);
         System.out.println(loan);
     }
+
+    @Test
+    public void testACMCalculate() {
+        ACMLoanCalculator calculator = new ACMLoanCalculator();
+        Loan loan = calculator.calLoan(
+                LoanUtil.totalLoanMoney(totalMoney, percent),
+                totalMonth,
+                LoanUtil.rate(rate, rateDiscount),
+                LoanUtil.RATE_TYPE_YEAR);
+        System.out.println(loan);
+    }
+
 }
